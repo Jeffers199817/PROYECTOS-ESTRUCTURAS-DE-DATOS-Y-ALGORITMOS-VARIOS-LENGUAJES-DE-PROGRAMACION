7ad579e6378fbdc338c130fb99cc3afa38ac1fa1
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-01-11 22:32:52 -0500

    INICIO DE ALGORITMOS Y ESTRUCTURA DE DATOS

---
+++ src/main/java/com/mycompany/algoritmos_y_estructura_de_datos/Algoritmos_y_estructura_de_datos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algoritmos_y_estructura_de_datos;
+
+/**
+ *
+ * @author JEFFERSON
+ */
+public class Algoritmos_y_estructura_de_datos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

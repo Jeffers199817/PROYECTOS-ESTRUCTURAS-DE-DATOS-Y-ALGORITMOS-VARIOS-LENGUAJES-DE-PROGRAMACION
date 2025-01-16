8955dbafd79ff294e85e1f3af7b65fcf07a3812f
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-01-15 21:42:21 -0500

    Algoritmo de ordenamiento BubbleSort

--- src/main/java/com/mycompany/algoritmos_y_estructura_de_datos/algoritmos/OrdenamientoBubbleSort.java
+++ src/main/java/com/mycompany/algoritmos_y_estructura_de_datos/algoritmos/OrdenamientoBubbleSort.java
@@ -2,4 +2,43 @@ package com.mycompany.algoritmos_y_estructura_de_datos.algoritmos;
 
 public class OrdenamientoBubbleSort {
 
+    public static void BubbleSort(int[] arr){
+
+        int n = arr.length;
+        boolean intercambio;
+
+        for(int i=0; i<n-1; i++) {
+            intercambio = false;
+            for( int j= 0 ; j<n-i-1; j++) {
+
+                if(arr[j]> arr[j+1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    intercambio = true;
+                }
+
+            }
+            if(!intercambio) break;
+        }
+
+
+    }
+
+
+    public static void main(String [] args){
+
+        int[] lista = {23,45,25,34,62,465,13,45,34,62};
+
+        BubbleSort(lista);
+        System.out.println("Lista Ordenada: ");
+
+        for(int i : lista) {
+            System.out.print( i + " ");
+        }
+
+
+    }
+
 }

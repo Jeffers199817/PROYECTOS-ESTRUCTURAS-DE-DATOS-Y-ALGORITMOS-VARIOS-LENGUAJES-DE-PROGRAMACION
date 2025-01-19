efb6f6cae0b027d22b593878952606928b8744e6
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-01-18 23:01:25 -0500

    Integración de la función mergeSort

--- src/main/java/com/mycompany/algoritmos_y_estructura_de_datos/algoritmos/OrdenamientoMergeSort.java
+++ src/main/java/com/mycompany/algoritmos_y_estructura_de_datos/algoritmos/OrdenamientoMergeSort.java
@@ -1,4 +1,17 @@
 package com.mycompany.algoritmos_y_estructura_de_datos.algoritmos;
 
 public class OrdenamientoMergeSort {
+
+    public static void mergeSort( int[] arr, int l, int r){
+
+        if(l<r){
+            int m = ( l+r)/2; // Encuentra el punto medio de la lista.
+        }
+    }
+
+
+
+    public static void main(String[] args){
+
+    }
 }
